2e4b380e9c412d58c330c36175c6ae056d8b72ed
rinalim <46272802+rinalim@users.noreply.github.com>
2019-11-20 01:35:09 +0900

    Update setup.py

--- PauseMenu/setup.py
+++ PauseMenu/setup.py
@@ -22,7 +22,7 @@ JS_EVENT_BUTTON = 0x01
 JS_EVENT_AXIS = 0x02
 JS_EVENT_INIT = 0x80
 
-PATH_PAUSEMODE = '/opt/retropie/configs/all/PauseMode/'
+PATH_PAUSEMENU = '/opt/retropie/configs/all/PauseMenu/'
 
 event_format = 'IhBB'
 event_size = struct.calcsize(event_format)
@@ -80,33 +80,46 @@ def process_event(event):
     if js_type & JS_EVENT_INIT:
         return -1
 
+    hex_chars = ""
+    if js_type == JS_EVENT_AXIS and js_number <= 7:
+        if js_number % 2 == 0:
+            if js_value <= JS_MIN * JS_THRESH:
+                hex_chars = axis_codes[0]
+            if js_value >= JS_MAX * JS_THRESH:
+                hex_chars = axis_codes[1]
+        if js_number % 2 == 1:
+            if js_value <= JS_MIN * JS_THRESH:
+                hex_chars = axis_codes[2]
+            if js_value >= JS_MAX * JS_THRESH:
+                hex_chars = axis_codes[3]
+    
     if js_type == JS_EVENT_BUTTON and js_value == 1:
         print ">> button index:", js_number
         return js_number
 
     return -1
 
-btn_up = -1
-btn_down = -1
+axis_up = ""
+axis_down = ""
 btn_select = -1
 btn_start = -1
-f = open(PATH_PAUSEMODE + "button.cfg", 'w')
+f = open(PATH_PAUSEMENU + "button.cfg", 'w')
 js_devs, js_fds = open_devices()
 
-print "Push a button for UP"
-while btn_up == -1:
+print "Move a lever for UP"
+while axis_up == "":
     for fd in js_fds:
         event = read_event(fd)
         if event:
-            btn_up = process_event(event)
+            axis_up = process_event(event)
     time.sleep(0.1)
     
-print "Push a button for DOWN"
-while btn_down == -1:
+print "Move a lever for DOWN"
+while axis_down == "":
     for fd in js_fds:
         event = read_event(fd)
         if event:
-            btn_down = process_event(event)
+            axis_down = process_event(event)
     time.sleep(0.1)
 
 print "Push a button for SELECT"
@@ -125,5 +138,5 @@ while btn_start == -1:
             btn_start = process_event(event)
     time.sleep(0.1)
 
-f.write(str(btn_up) + "\n" + str(btn_down) + "\n" + str(btn_select) + " " + str(btn_start))
+f.write(axis_up + "\n" + axis_down + "\n" + str(btn_select) + "\n" + str(btn_start))
 f.close()
