f62b4baa15685c17e9d260c7019c6c84bcbcaf51
rinalim <46272802+rinalim@users.noreply.github.com>
2019-11-20 01:14:48 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,16 +4,16 @@
 
 sudo cp ./libraspidmx.so.1 /usr/lib
 
-rm -rf /opt/retropie/configs/all/VolumeJoy/
-mkdir /opt/retropie/configs/all/VolumeJoy/
-cp -f -r ./VolumeJoy /opt/retropie/configs/all/
+rm -rf /opt/retropie/configs/all/PauseMenu/
+mkdir /opt/retropie/configs/all/PauseMenu/
+cp -f -r ./PauseMenu /opt/retropie/configs/all/
 
-sudo chmod 755 /opt/retropie/configs/all/VolumeJoy/pngvolume
+sudo chmod 755 /opt/retropie/configs/all/PauseMenu/omxiv-pause
 
-sudo sed -i '/volume.py/d' /opt/retropie/configs/all/autostart.sh
-sudo sed -i '1i\\/usr/bin/python /opt/retropie/configs/all/VolumeJoy/volume.py /dev/input/js0 &' /opt/retropie/configs/all/autostart.sh
+sudo sed -i '/PauseMenu.py/d' /opt/retropie/configs/all/autostart.sh
+sudo sed -i '1i\\/usr/bin/python /opt/retropie/configs/all/PauseMenu/PauseMenu.py /dev/input/js0 &' /opt/retropie/configs/all/autostart.sh
 
-python ./VolumeJoy/setup.py /dev/input/js0
+python ./PauseMenu/setup.py /dev/input/js0
 
 echo
 echo "Setup Completed. Reboot after 3 Seconds."
