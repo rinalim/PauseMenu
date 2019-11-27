719f0963975bad343e6902c08a96d2659f82a70a
rinalim <46272802+rinalim@users.noreply.github.com>
2019-11-28 01:03:38 +0900

    Update PauseMenu.py

--- PauseMenu/PauseMenu.py
+++ PauseMenu/PauseMenu.py
@@ -515,6 +515,8 @@ def main():
                 time.sleep(1)    # wait for launching game
 		print "wait for launching game"
                 continue
+            else:
+                break
         system = run_cmd("ps -ef | grep emulators | grep -v grep | awk '{print $10}'").split("/")[4]
         romname = run_cmd("ps -ef | grep emulators | grep -v grep | awk '{print $13}'").split("/")[6][0:-5]
         if check_update() == True:
