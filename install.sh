e88590b96a395d42a2f5e306300055daea885cce
rinalim <46272802+rinalim@users.noreply.github.com>
2020-01-11 01:09:44 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ cp -f -r ./PauseMenu /opt/retropie/configs/all/
 sudo chmod 755 /opt/retropie/configs/all/PauseMenu/omxiv-pause
 
 sudo sed -i '/PauseMenu.py/d' /opt/retropie/configs/all/runcommand-onstart.sh
-echo '/usr/bin/python /opt/retropie/configs/all/PauseMenu/PauseMenu.py /dev/input/js0 -control &' >> /opt/retropie/configs/all/runcommand-onstart.sh
+echo '/usr/bin/python /opt/retropie/configs/all/PauseMenu/PauseMenu.py /dev/input/js0 &' >> /opt/retropie/configs/all/runcommand-onstart.sh
 
 chgrp -R -v pi /opt/retropie/configs/all/PauseMenu/
 chown -R -v pi /opt/retropie/configs/all/PauseMenu/
