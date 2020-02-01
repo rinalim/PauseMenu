11561a41bf2e29ccb38e36fc993f26861f040d2c
rinalim <ooll00@naver.com>
2020-02-02 01:17:42 +0900

    Update PauseMenu.py

--- experimental/PauseMenu.py
+++ experimental/PauseMenu.py
@@ -628,11 +628,12 @@ def send_hotkey(key):
     
 def save_picture():
     path = "/opt/retropie/configs/all/retroarch/screenshots/"
-    os.system("sudo rm " + path + "*")
     while True:
         file_list = os.listdir(path)
         if len(file_list) > 0:
             break
+        else:
+            time.sleep(0.1)
     print file_list[0]
         
     
@@ -753,6 +754,7 @@ def process_event(event):
                         time.sleep(0.1)
                         send_hotkey("f8")
                         stop_viewer()
+                        os.system("sudo rm /opt/retropie/configs/all/retroarch/screenshots/*")
                         save_picture()
                         PAUSE_MODE_ON = False
                     elif MENU_INDEX == 5:
