a5fb1966bc09565c114fbe5aeefbc3fdb3ed1a03
rinalim <ooll00@naver.com>
2020-02-01 21:49:23 +0900

    Update PauseMenu.py

--- experimental/PauseMenu.py
+++ experimental/PauseMenu.py
@@ -739,6 +739,8 @@ def process_event(event):
                     elif MENU_INDEX == 4:
                         #print "Save"
                         os.system("ps -ef | grep emulators | grep -v grep | awk '{print $2}' | xargs kill -SIGCONT &")
+                        send_hotkey("p")
+                        time.sleep(0.1)
                         send_hotkey("f8")
                         time.sleep(0.1)
                         send_hotkey("f2")
