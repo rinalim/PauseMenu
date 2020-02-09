129133b0ae2b7bf1706f37962559d28da0fe34d0
rinalim <ooll00@naver.com>
2020-02-09 19:33:12 +0900

    Update PauseMenu.py

--- experimental/PauseMenu.py
+++ experimental/PauseMenu.py
@@ -589,7 +589,7 @@ def start_viewer():
 def start_viewer_osd():
     if is_running("omxiv-pause") == False:
         if VIEW_MODE == "full" and os.path.isfile(PATH_PAUSEMENU + "images/control" + romname + "_osd.png") == True :
-            echo_file(PATH_PAUSEMENU + "images/control" + romname + "_osd.png", "/tmp/pause_osd.txt". "r")
+            echo_file(PATH_PAUSEMENU + "images/control" + romname + "_osd.png", "/tmp/pause_osd.txt", "r")
             os.system(VIEWER_OSD + get_location() +" &")
 
 def start_viewer_saving():
