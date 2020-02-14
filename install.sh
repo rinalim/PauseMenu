ee1de0d01387ae81ad0428b30787947398101ce8
rinalim <ooll00@naver.com>
2020-02-15 02:53:26 +0900

    shell script

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ echo 'sudo /usr/bin/python /opt/retropie/configs/all/PauseMenu/PauseMenu.py /dev
 sudo sed -i '/PauseMenu.py/d' /opt/retropie/configs/all/runcommand-onend.sh
 echo 'sudo pkill -ef PauseMenu.py' >> /opt/retropie/configs/all/runcommand-onend.sh
 
-chgrp -R -v pi /opt/retropie/configs/all/PauseMenu/
-chown -R -v pi /opt/retropie/configs/all/PauseMenu/
+chgrp -R -v pi /opt/retropie/configs/all/PauseMenu/ > /dev/null 2>&1
+chown -R -v pi /opt/retropie/configs/all/PauseMenu/ > /dev/null 2>&1
 
 python ./PauseMenu/setup.py /dev/input/js0
