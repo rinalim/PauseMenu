95a8f85a40159e7acea5cc3bd49375c0849cf191
rinalim <ooll00@naver.com>
2020-02-14 15:02:49 +0900

    Create update.sh

---
+++ experimental/update.sh
@@ -0,0 +1,17 @@
+# Reference    :
+# https://github.com/RetroPie/RetroPie-Setup/blob/master/scriptmodules/supplementary/runcommand/joy2key.py
+# https://github.com/sana2dang/PauseMode
+
+mkdir ./backup
+cp -f -r /opt/retropie/configs/all/PauseMenu/image/save/* ./backup
+
+rm -rf /opt/retropie/configs/all/PauseMenu/
+mkdir /opt/retropie/configs/all/PauseMenu/
+cp -f -r ./PauseMenu /opt/retropie/configs/all/
+
+cp -f -r ./backup/* /opt/retropie/configs/all/PauseMenu/image/save
+
+sudo chmod 755 /opt/retropie/configs/all/PauseMenu/omxiv-pause
+
+chgrp -R -v pi /opt/retropie/configs/all/PauseMenu/
+chown -R -v pi /opt/retropie/configs/all/PauseMenu/
