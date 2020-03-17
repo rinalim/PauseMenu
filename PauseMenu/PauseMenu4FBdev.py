3aa5b7714c75d4b883deaaf843127995fac7d53a
rinalim <ooll00@naver.com>
2020-03-17 21:17:24 +0900

    Update PauseMenu4FBdev.py

--- PauseMenu/PauseMenu4FBdev.py
+++ PauseMenu/PauseMenu4FBdev.py
@@ -986,11 +986,9 @@ def fbdev_setup():
     
     if VIEW_MODE == "fba" or VIEW_MODE == "libretro":
         images_resume = img_paste(images_bg,
-            Image.open(PATH_PAUSEMENU + "images/" + VIEW_MODE + "_resume.png")
-            .paste(PATH_PAUSEMENU + "images/control/" + submenu + "_layout0.png", (0,0)))
+            Image.open(PATH_PAUSEMENU + "images/" + VIEW_MODE + "_resume.png").paste(PATH_PAUSEMENU + "images/control/" + submenu + "_layout0.png", (0,0)))
         images_stop = img_paste(images_bg,
-            Image.open(PATH_PAUSEMENU + "images/" + VIEW_MODE + "_stop.png")
-            .paste(PATH_PAUSEMENU + "images/control/" + submenu + "_layout0.png", (0,0)))
+            Image.open(PATH_PAUSEMENU + "images/" + VIEW_MODE + "_stop.png").paste(PATH_PAUSEMENU + "images/control/" + submenu + "_layout0.png", (0,0)))
     else:
         images_resume = img_paste(images_bg,
             Image.open(PATH_PAUSEMENU + "images/" + VIEW_MODE + "_resume.png"))
