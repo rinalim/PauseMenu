8b25e11959e57b15dce6ff32a8a787163d7612cc
rinalim <46272802+rinalim@users.noreply.github.com>
2020-12-30 18:03:17 +0900

    Update install-rpi.sh

--- install-rpi.sh
+++ install-rpi.sh
@@ -5,7 +5,7 @@
 sudo apt install libjpeg8 imagemagick  -y
 sudo apt install fonts-nanum fonts-nanum-extra -y
 sudo apt install python3-pil python3-pyudev -y
-sudo pip install keyboard
+sudo pip3 install keyboard
 
 rm -rf /opt/retropie/configs/all/PauseMenu/
 mkdir /opt/retropie/configs/all/PauseMenu/
@@ -14,7 +14,7 @@ cp -f -r ./PauseMenu /opt/retropie/configs/all/
 sudo chmod 755 /opt/retropie/configs/all/PauseMenu/omxiv-pause
 
 sudo sed -i '/PauseMenu.py/d' /opt/retropie/configs/all/runcommand-onstart.sh
-echo 'sudo /usr/bin/python /opt/retropie/configs/all/PauseMenu/PauseMenu.py /dev/input/js0 -full &' >> /opt/retropie/configs/all/runcommand-onstart.sh
+echo 'sudo /usr/bin/python3 /opt/retropie/configs/all/PauseMenu/PauseMenu.py /dev/input/js0 -full &' >> /opt/retropie/configs/all/runcommand-onstart.sh
 sudo sed -i '/PauseMenu.py/d' /opt/retropie/configs/all/runcommand-onend.sh
 echo 'sudo pkill -ef PauseMenu.py > /dev/null 2>&1' >> /opt/retropie/configs/all/runcommand-onend.sh
 
