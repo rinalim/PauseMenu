95fe12bb47fefdceb6381e5c715d75aaf99c7063
rinalim <46272802+rinalim@users.noreply.github.com>
2020-12-30 18:11:56 +0900

    Update install-rpi-simple.sh

--- install-rpi-simple.sh
+++ install-rpi-simple.sh
@@ -3,7 +3,7 @@
 # https://github.com/sana2dang/PauseMode
 
 sudo apt install libjpeg8 -y
-sudo apt install python-pyudev -y
+sudo apt install python3-pyudev -y
 
 rm -rf /opt/retropie/configs/all/PauseMenu/
 mkdir /opt/retropie/configs/all/PauseMenu/
@@ -12,11 +12,11 @@ cp -f -r ./PauseMenu /opt/retropie/configs/all/
 sudo chmod 755 /opt/retropie/configs/all/PauseMenu/omxiv-pause
 
 sudo sed -i '/PauseMenu.py/d' /opt/retropie/configs/all/runcommand-onstart.sh
-echo 'sudo /usr/bin/python /opt/retropie/configs/all/PauseMenu/PauseMenu.py /dev/input/js0 &' >> /opt/retropie/configs/all/runcommand-onstart.sh
+echo 'sudo /usr/bin/python3 /opt/retropie/configs/all/PauseMenu/PauseMenu.py /dev/input/js0 &' >> /opt/retropie/configs/all/runcommand-onstart.sh
 sudo sed -i '/PauseMenu.py/d' /opt/retropie/configs/all/runcommand-onend.sh
 echo 'sudo pkill -ef PauseMenu.py' >> /opt/retropie/configs/all/runcommand-onend.sh
 
 chgrp -R -v pi /opt/retropie/configs/all/PauseMenu/ > /dev/null 2>&1
 chown -R -v pi /opt/retropie/configs/all/PauseMenu/ > /dev/null 2>&1
 
-python ./PauseMenu/setup.py /dev/input/js0
+python3 ./PauseMenu/setup.py /dev/input/js0
