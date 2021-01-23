1c14332a92ca77f412d5103621cc6612bac5743a
rinalim <46272802+rinalim@users.noreply.github.com>
2021-01-24 02:32:35 +0900

    Update install-emu.sh

--- install-emu.sh
+++ install-emu.sh
@@ -2,9 +2,8 @@ pip3 install pyudev
 
 cp -f -r ./PauseMenu /opt/
 
-#sudo sed -i '/PauseMenu.py/d' /opt/retropie/configs/all/runcommand-onstart.sh
-#echo 'sudo /usr/bin/python3 /opt/retropie/configs/all/PauseMenu/PauseMenu.py /dev/input/js0 &' >> /opt/retropie/configs/all/runcommand-onstart.sh
-#sudo sed -i '/PauseMenu.py/d' /opt/retropie/configs/all/runcommand-onend.sh
-#echo 'sudo pkill -ef PauseMenu.py' >> /opt/retropie/configs/all/runcommand-onend.sh
-
 /opt/bin/python3 ./PauseMenu/setup-emu.py /dev/input/js0
+
+echo "Add "
+echo "opt/bin/python3 /opt/PauseMenu/PauseMenu4Emu.py /dev/input/js0 &"
+echo "to /storage/.config/custom_start.sh"
