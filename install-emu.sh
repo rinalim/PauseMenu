e1fc7bfc7e8af3aef835d90bb0352e5297a2c8a3
rinalim <46272802+rinalim@users.noreply.github.com>
2021-01-24 02:34:46 +0900

    Update install-emu.sh

--- install-emu.sh
+++ install-emu.sh
@@ -5,4 +5,5 @@ cp -f -r ./PauseMenu /opt/
 /opt/bin/python3 ./PauseMenu/setup-emu.py /dev/input/js0
 
 echo "Add the follwing line to /storage/.config/custom_start.sh"
-echo "opt/bin/python3 /opt/PauseMenu/PauseMenu4Emu.py /dev/input/js0 &"
+echo "/opt/bin/python3 /opt/PauseMenu/PauseMenu4Emu.py /dev/input/js0 &"
+echo ""
