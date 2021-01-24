4ad6e05242c6df5e448da638e7b1f1d93edee217
rinalim <46272802+rinalim@users.noreply.github.com>
2021-01-24 15:49:42 +0900

    Update install-emu.sh

--- install-emu.sh
+++ install-emu.sh
@@ -2,6 +2,8 @@ pip3 install pyudev
 
 cp -f -r ./PauseMenu /opt/
 
+sudo chmod 755 /opt/PauseMenu/fbdump
+
 /opt/bin/python3 ./PauseMenu/setup-emu.py /dev/input/js0
 
 echo "Add the follwing line to /storage/.config/custom_start.sh"
