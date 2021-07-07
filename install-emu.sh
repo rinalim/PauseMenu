ba412ba16b5f5e15945745eb8bf8585e2b4a5bbb
rinalim <ooll00@naver.com>
2021-07-08 00:52:30 +0900

    Update install-emu.sh

--- install-emu.sh
+++ install-emu.sh
@@ -3,10 +3,12 @@ pip3 install pyudev
 cp -f -r ./PauseMenu /opt/
 
 if [[ ! -f /usr/bin/fbdump ]]; then
-    echo "Copy fbdump"
-    cp /opt/PauseMenu/fbdump-v1 /opt/bin/fbdump
-    chmod 755 /opt/bin/fbdump
+    cp /opt/PauseMenu/fbdump /opt/bin/fbdump
+else
+    cp /usr/bin/fbdump /opt/bin/fbdump
 fi
+chmod 755 /opt/bin/fbdump
+
 /opt/bin/python3 ./PauseMenu/setup-emu.py /dev/input/js0
 
 echo "Add the follwing line to /storage/.config/custom_start.sh"
